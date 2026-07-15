60d428e96eed258ac104b239b4aac43c657ed58b
Andrew Lawrence <lawrenceajl@gmail.com>
2026-07-15 17:10:50 +0100

    [REFACTOR] generic calculator-merger

--- R/nullmodel.R
+++ R/nullmodel.R
@@ -1,33 +1,60 @@
+
+null_model_mse_split <- function(x, train, test, ...) {
+  UseMethod("null_model_mse_split")
+}
+
+#' @export
+null_model_mse_split.regression <- function(x, train, test, ...) {
+  y <- x$y
+
+  y_hat <- mean(y[train])
+  mean((y[test] - y_hat)^2)
+}
+
+#' @export
+null_model_mse_split.classification <- function(x, train, test, event_level = NULL, ...) {
+  y <- outcome01(x, event_level = event_level)
+
+  y_hat <- mean(y[train])
+  mean((y[test] - y_hat)^2)
+}
+
+
 #' Null model performance
 #'
-#' Calculates the performance of a null model for a problem.
-#'
-#' For regression, the null model predicts the mean of y for every observation
-#' and returns the mean squared error.
-#'
-#' For classification, the null model predicts p(event) at the prevalence and
-#' returns the mean squared error (i.e. updated Brier score)
+#' Calculates null model mean squared error using either apparent validity
+#' or cross-validation folds stored in the problem object.
 #'
 #' @param x A problem object.
+#' @param validity Either "apparent" or "cv".
+#' @param combine Function used to combine split-level estimates.
 #' @param ... Additional arguments passed to methods.
 #'
 #' @return Numeric scalar.
 #'
 #' @export
-null_model_mse <- function(x, ...) {
+null_model_mse <- function(
+    x,
+    validity = c("apparent", "cv"),
+    combine = combine_mean,
+    ...
+) {
   UseMethod("null_model_mse")
 }
 
-#' @export
-null_model_mse.regression <- function(x, ...) {
-  y <- x$y
-  y_hat <- mean(y)
-  mean((y - y_hat)^2)
-}
 
 #' @export
-null_model_mse.classification <- function(x, ...) {
-  y <- as.numeric(x$y) - 1
-  y_hat <- mean(y)
-  mean((y - y_hat)^2)
+null_model_mse.problem <- function(
+    x,
+    validity = c("apparent", "cv"),
+    combine = combine_mean,
+    ...
+) {
+  estimate_by_validity(
+    x = x,
+    validity = validity,
+    estimator = null_model_mse_split,
+    combine = combine,
+    ...
+  )
 }
