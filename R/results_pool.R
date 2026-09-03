1ae678ee7ac03771d399d0c8c27ca4bd6211e289
Andrew Lawrence <lawrenceajl@gmail.com>
2026-09-03 18:06:33 +0100

    [FEAT] additional plotting options for tuning plots

--- R/results_pool.R
+++ R/results_pool.R
@@ -73,10 +73,21 @@ plot_pooled_results <- function(x) {
 #'     or logged to disk by a [cvmultipredict] model.
 #' @param se - flag: include error bars for +/- 1SD of performance?
 #'     SD is calculated over the tuning CV resamples.
-#'
+#' @param highlight_bayes a flag, if `TRUE` then bayesian optimisation
+#'     steps get a colour scale.
+#' @param highlight_best a flag, if `TRUE` then the "best" hyperparameter
+#'     combination is highlighted with a red circle.
+#' @param alpha transparency used for plotting.
+#' @param best_function a function applied to a numeric vector (including NAs)
+#'     that returns the "best" value (usually min or max). Must ignore NA values.
 #' @return A [ggplot object][ggplot2::ggplot]
 #' @export
-plot_tuning_results <- function(x, se = FALSE) {
+plot_tuning_results <- function(x,
+                                se = FALSE,
+                                highlight_bayes = TRUE,
+                                highlight_best = TRUE,
+                                alpha = 1.0,
+                                best_function = \(x) min(x, na.rm = TRUE)) {
   check_suggested("tidyr", "Plotting")
   check_suggested("ggplot2", "Plotting")
   checkmate::assert_data_frame(x, col.names = "named")
@@ -95,6 +106,10 @@ plot_tuning_results <- function(x, se = FALSE) {
       x[[i]] <- log10(x[[i]])
     }
   }
+  # NA-hack for colour scale if highlight_bayes:
+  if (".iter" %in% colnames(x) && highlight_bayes) {
+    x$.iter <- ifelse(x$.iter == 0L, NA, x$.iter)
+  }
 
   p <- x |>
     tidyr::pivot_longer(cols = 1:idx,
@@ -106,16 +121,32 @@ plot_tuning_results <- function(x, se = FALSE) {
                                  ymax = .data$mean + .data$std_err))
 
   if ( se ) {
-    p <- p + ggplot2::geom_errorbar()
+    p <- p + ggplot2::geom_errorbar(alpha = alpha)
   }
 
-  if (".iter" %in% colnames(x)) {
+  if (".iter" %in% colnames(x) && highlight_bayes) {
     p <- p +
-      ggplot2::geom_point(ggplot2::aes(colour = .data$.iter))
+      ggplot2::geom_point(ggplot2::aes(colour = .data$.iter),
+                          alpha = alpha) +
+      ggplot2::scale_colour_gradient(na.value = "black",
+                                     high = "lightblue",
+                                     low = "darkblue")
   } else {
-    p <- p + ggplot2::geom_point()
+    p <- p + ggplot2::geom_point(alpha = alpha)
   }
-
+  if ( highlight_best ) {
+    best_sel <- (x[["mean"]] - best_function(x[["mean"]])) <
+      (2 * .Machine$double.eps)
+    best <- x[best_sel, ] |>
+      tidyr::pivot_longer(cols = 1:idx,
+                          names_to = "hyperparameter",
+                          values_to = "hyperparameter_value")
+    p <- p +
+      ggplot2::geom_point(data = best,
+                          colour = "red",
+                          size = 3,
+                          shape = 21)
+  }
   p <- p +
     ggplot2::facet_wrap( ~ .data$hyperparameter, scales = "free_x")
   p
