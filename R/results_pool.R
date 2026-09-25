709a3640237f9c84e0ba8d926c63d15e75eaf549
Andrew Lawrence <lawrenceajl@gmail.com>
2026-09-25 14:24:46 +0100

    [FEAT] additional/alternative pooled standard errors for repeated k-fold CV.

--- R/results_pool.R
+++ R/results_pool.R
@@ -5,8 +5,27 @@
 #'     Input `x` should be the return of cvmultipredict.
 #'     i.e. a named list of results data.frames - one for each model.
 #'
+#' @section Repetition Standard Errors:
+#' These require sufficient repeats of cross-validation to estimate the
+#' standard deviation over these repeats. Typically at least 10 x k-fold
+#' with better `.rep_std_err` estimates at 100 x k-fold.
+#'
+#' It is a bad idea to combine `na.rm=TRUE` and
+#' `use_repetition=TRUE`. In the event that some models are returning `NA` values
+#' (e.g. failing to converge) then the per-repetition standard deviation
+#' will not cover all the folds for each repetition and the
+#' `.rep_std_err` will not have the desired interpretation. `NA`s should either
+#' propagate (`na.rm = FALSE`) or the fold-fold `.std_err` be used.
+#'
 #' @param x [cvmultipredict] result.
 #' @inheritParams base::mean na.rm
+#' @param use_repetition Return an additional SD estimate (`.rep_std_err`)
+#'     calculated for repetitions of k-fold CV rather than over individual folds.
+#' @param repetition_regexp A [regular expression] used to extract unique
+#'     identifiers of each CV-repetition from cross-validation labels.
+#'     For example, the default regexp `"Rep[0-9]+"` matches substrings
+#'     like `"Rep001"`, `"Rep01"`, or `"Rep1"`, and so works with labels
+#'     formatted like `"Rep001.Fold01"`.
 #'
 #' @return A tibble containing columns:
 #'
@@ -15,25 +34,75 @@
 #'         or apparent.
 #'  * `.metric`: the [metric][yardstick::metrics].
 #'  * `.estimate`: metric value (for CV averaged over resamples)
-#'  * `.std_err`: SD of the metric value (`NA` for apparent performance)
-#'
+#'  * `.std_err`: For cross-validated values, contains the SD of the
+#'      metric value (`NA` for apparent performance).
+#'  * `.nreps` : For repeated k-fold, when `use_repetition=TRUE` will
+#'      contain the number of repetitions detected.
+#'  * `.rep_std_err` : When `use_repetition=TRUE`the standard deviation
+#'      of the metric taken after averaging over the k-folds in each
+#'      repetition - i.e. a variability measure for the random division
+#'      into folds which does not include fold-fold variability.
 #' @export
-pool_results <- function(x, na.rm = FALSE) {
+pool_results <- function(x,
+                         na.rm = FALSE,
+                         use_repetition = FALSE,
+                         repetition_regexp = "Rep[0-9]+") {
   check_suggested("tidyr", "Results Pooling")
+
   if (!inherits(x, "data.frame")) {
     x <- bind_rows(x, .id = "model")
   }
-  x |>
+
+  # retain label until repetition summaries are computed
+  x <- x |>
     dplyr::mutate(cvtype = factor(
       ifelse(.data$label == "apparent", "app", "cv"),
       levels = c("app", "cv")
-    )) |>
-    dplyr::select(-c("label", ".estimator")) |>
+    ))
+
+  out <- x |>
     tidyr::nest(.by = c("model", "cvtype", ".metric")) |>
     dplyr::mutate(
       .estimate = purrr::map_dbl(.data$data, ~ mean(.x$.estimate, na.rm = na.rm)),
       .std_err = purrr::map_dbl(.data$data, ~ sd(.x$.estimate, na.rm = na.rm))
-    ) |>
+    )
+
+  if (use_repetition) {
+
+    out <- out |>
+      dplyr::mutate(
+        .nreps = purrr::map_int(.data$data, function(df) {
+          df <- dplyr::filter(df, .data$label != "apparent")
+
+          if (nrow(df) == 0) {
+            return(NA_integer_)
+          }
+
+          reps <- stringr::str_extract(df$label, repetition_regexp)
+
+          dplyr::n_distinct(reps)
+        }),
+
+        .rep_std_err = purrr::map_dbl(.data$data, function(df) {
+          df <- dplyr::filter(df, .data$label != "apparent")
+
+          if (nrow(df) == 0) {
+            return(NA_real_)
+          }
+
+          df <- df |>
+            dplyr::mutate(.rep = stringr::str_extract(.data$label,
+                                                      repetition_regexp)) |>
+            dplyr::group_by(.data$.rep) |>
+            dplyr::summarise(.estimate = mean(.data$.estimate, na.rm = na.rm),
+                             .groups = "drop")
+
+          stats::sd(df$.estimate, na.rm = na.rm)
+        })
+      )
+  }
+
+  out |>
     dplyr::select(-"data")
 }
 
